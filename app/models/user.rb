06f33e4948b9665aaf6d17f9ff8dbb40f56c5b85
Hiro-KT <kh01210237.khko@gmail.com>
2021-02-08 16:31:52 +0900

    [Layout] Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,2 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
-   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_061941) do
+ActiveRecord::Schema.define(version: 2021_02_08_065546) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.string "image"
+    t.boolean "is_admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
