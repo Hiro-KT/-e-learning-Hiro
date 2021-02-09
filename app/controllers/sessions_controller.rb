7d39042ad3359e0cb0689bd51e504c75e37f37d4
Hiro-KT <kh01210237.khko@gmail.com>
2021-02-09 16:26:36 +0900

    [Function]Log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+        login(@user)
+        redirect_to root_url
+    else
+        render 'new'
+    end
+  end
+
+  def destroy
+    logout(@user)
+    redirect_to root_url
+  end
 end
