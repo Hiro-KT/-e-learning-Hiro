e2bc57285144502c9882cb40083b8b4b87400d3f
Hiro-KT <78471137+Hiro-KT@users.noreply.github.com>
2021-02-09 16:31:34 +0900

    [Function]Log in (#6)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+  def login(user)
+    session[:user_id] = user_id
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logout(user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root   "pages#home"
-  
-  
   resources :users
   resources :sessions
 end
