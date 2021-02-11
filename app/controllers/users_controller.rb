f13cf214bf910c665426999236faec449df40b5a
Hiro-KT <kh01210237.khko@gmail.com>
2021-02-11 16:58:33 +0900

    remove @user.update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,6 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
 
     if @user.update(user_params)
       redirect_to @user
