d7a6937d33d06749f11fed9593a403d3564cfcd2
Hiro-KT <kh01210237.khko@gmail.com>
2021-02-15 17:49:10 +0900

    change routes and path name,remove methods in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root   "pages#home"
+  resources :users
+  resources :sessions
 
   namespace :admin do
     resources :users
   end
-  
-  resources :sessions
 
   delete '/logout', to: 'sessions#destroy'
 end
