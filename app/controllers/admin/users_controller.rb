744cdd40cf910da11168dad9b961e49b00bf9ee2
Hiro-KT <kh01210237.khko@gmail.com>
2021-02-16 16:34:50 +0900

    [Function]Admin users Add and Remove

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,4 +4,19 @@ class Admin::UsersController < ApplicationController
     @users = User.all
   end
 
+  def add_admin
+    @user = User.find(params[:id])
+    @user.update(is_admin: true)
+  end
+
+  def remove_admin
+    @user = User.find(params[:id])
+    @user.update(is_admin: false)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
