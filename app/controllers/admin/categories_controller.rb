2c765cb8752a778d0c218cae2916b774ea3f40cd
Hiro-KT <kh01210237.khko@gmail.com>
2021-02-17 16:28:07 +0900

    [Function]Category Create

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -7,4 +7,19 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title,:description)
+  end
+
 end
