744537fb53ec009ec8c0c2fcfcc9a3045b29cac7
Hiro-KT <78471137+Hiro-KT@users.noreply.github.com>
2021-02-17 18:53:29 +0900

    [Layout]Category edit (#17)
    * [Layout]Category edit
    
    * change submit name

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -18,6 +18,21 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      redirect_to admin_categories_url
+    else
+      render :edit
+    end
+
+  end
+
   private
   def category_params
     params.require(:category).permit(:title,:description)
