e3091b0ef890a05638637f7eec10e88e1938f7cc
Hiro-KT <78471137+Hiro-KT@users.noreply.github.com>
2021-02-19 18:57:16 +0900

    [Function]Word update (#22)

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -19,8 +19,20 @@ class Admin::WordsController < ApplicationController
   end
 
   def edit
-    @category = Category.find_by_id(params[:category_id])
+    @category = Category.find(params[:category_id])
     @word = Word.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:category_id])
+    @word = Word.find(params[:id])
+
+    if @word.update(word_params)
+      redirect_to admin_category_url(@category)
+    else
+      render :edit
+    end
+
   end
 
   def word_params
