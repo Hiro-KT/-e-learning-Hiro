6be13efacd136fa3a0e48091be988799db3a44ef
Hiro-KT <78471137+Hiro-KT@users.noreply.github.com>
2021-02-19 17:43:48 +0900

    [Function]Create word (#20)
    * [Function]Create word
    
    * [Function]Word create
    
    * change some wrongs in controller

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,9 +1,25 @@
 class Admin::WordsController < ApplicationController
 
+
   def new
     @category = Category.find_by_id(params[:category_id])
     @word = Word.new
     3.times { @word.choices.build }
+  end
+
+  def create
+    @category = Category.find_by_id(params[:category_id])
+    @word = @category.words.new(word_params)
+
+    if @word.save
+      redirect_to admin_category_url(@category)
+    else
+      render 'new'
+    end
+  end
+
+  def word_params
+    params.require(:word).permit(:content, choices_attributes: [ :id,  :content, :is_correct ] )
   end
 
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -2,4 +2,10 @@ class Word < ApplicationRecord
   belongs_to :category
   has_many :choices
   accepts_nested_attributes_for :choices
+
+  def correct_answer
+    # use "find_by" method for "choices" to find the choice where "is_correct" is true
+    choices.find_by(is_correct: true)
+  end
+
 end
