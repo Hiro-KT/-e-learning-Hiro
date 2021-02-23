7c65a8eae8c1657fd54d48159cd5d6d9e6a309a2
Hiro-KT <kh01210237.khko@gmail.com>
2021-02-23 18:28:44 +0900

    change page_new,indent in controller, add belongs_to :user

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,9 @@
 class LessonsController < ApplicationController
 
   def create
-      @lesson = current_user.lessons.new(category_id: params[:category_id])
-      @lesson.save
-      redirect_to new_lesson_answer_url(@lesson)
+    @lesson = current_user.lessons.new(category_id: params[:category_id])
+    @lesson.save
+    redirect_to new_lesson_answer_url(@lesson)
   end
 
   def show
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,5 @@
 class Lesson < ApplicationRecord
+  belongs_to :user
   belongs_to :category
   has_many :answers, dependent: :destroy
   has_many :words, through: :answers
