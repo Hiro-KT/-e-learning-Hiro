49b8e759fc2789fe85976865a6b8c72668e5a522
Hiro-KT <kh01210237.khko@gmail.com>
2021-02-26 18:51:09 +0900

    [Function]Follow and unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,33 @@
 class User < ApplicationRecord
   has_many :lessons
+  has_many :relationships
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }
 
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_nil: true
+
+  def follow(other_user)
+    Relationship.create(
+      follower_id: id,
+      followed_id: other_user.id
+    )
+  end
+
+  def relationship(other_user)
+    Relationship.find_by(
+      follower_id: id,
+      followed_id: other_user.id
+    )
+  end
+
+  def active_relationships
+    Relationship.where(follower_id: id)   #get all Relationship where current user is FOLLOWER
+  end
+
+  def following
+    ids = active_relationships.pluck(:followed_id)      #get all FOLLOWER ID
+    User.where(id: ids)                                 #get all FOLLOWER USERS wwhere ID id in ID LIST
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_083921) do
+ActiveRecord::Schema.define(version: 2021_02_26_083616) do
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "lesson_id"
